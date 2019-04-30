a901d01701d574afbfd98ea1405027651a4e4a59
cherry <cookie@chaeyeon-ui-MacBook-Pro.local>
2019-04-30 22:38:56 +0900

    Add api method

--- src/main/java/org/cnu/realcoding/riotapi/api/RiotApi.java
+++ src/main/java/org/cnu/realcoding/riotapi/api/RiotApi.java
@@ -1,4 +1,22 @@
 package org.cnu.realcoding.riotapi.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class RiotApi {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private String requestSummonerNameUrl = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={apiKey}";
+    private String requestEncryptedSummonerIdUrl="https://kr.api.riotgames.com/lol/league/v4/positions/by-summoner/{encryptedSummonerId}?api_key={apiKey}";
+
+    public String getSummonerName(String userName) {
+        return restTemplate.exchange(requestSummonerNameUrl, HttpMethod.GET, null, String.class, userName, "RGAPI-733e4766-5acf-4583-8c9f-9cba2ba6e9f5").getBody();
+    }
+    public String getEncryptedSummonerId(String encryptedId){
+        return restTemplate.exchange(requestEncryptedSummonerIdUrl, HttpMethod.GET, null, String.class, encryptedId, "RGAPI-733e4766-5acf-4583-8c9f-9cba2ba6e9f5").getBody();
+    }
 }
