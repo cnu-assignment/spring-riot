b01f247195b9daf44b2baa7f05c4067949e11fb4
Yongjae Yi <dididy@knou.ac.kr>
2019-04-30 22:46:21 +0900

    Make DataRepository class

--- src/main/java/org/cnu/realcoding/riotapi/domain/EncryptedSummonerId.java
+++ src/main/java/org/cnu/realcoding/riotapi/domain/EncryptedSummonerId.java
@@ -1,4 +1,9 @@
 package org.cnu.realcoding.riotapi.domain;
 
+import lombok.Data;
+
+@Data
 public class EncryptedSummonerId {
+
+
 }
--- src/main/java/org/cnu/realcoding/riotapi/repository/DataRepository.java
+++ src/main/java/org/cnu/realcoding/riotapi/repository/DataRepository.java
@@ -1,6 +1,18 @@
 // DB쪽 호출하는 녀석들
 package org.cnu.realcoding.riotapi.repository;
 
+import org.cnu.realcoding.riotapi.domain.EncryptedSummonerId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Repository;
+
 @Repository
-public class Repository {
+public class DataRepository {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public void getAndInsertDB(EncryptedSummonerId encryptedSummonerId) {
+        this.mongoTemplate.insert(encryptedSummonerId);
+    }
+
 }
