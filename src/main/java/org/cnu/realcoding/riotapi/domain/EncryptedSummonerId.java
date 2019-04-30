50397896f27ef88397f108fa3752a5de6c8d0493
jihyun <jhhhj1002@hanmail.net>
2019-05-01 00:08:42 +0900

    domain

--- src/main/java/org/cnu/realcoding/riotapi/domain/EncryptedSummonerId.java
+++ src/main/java/org/cnu/realcoding/riotapi/domain/EncryptedSummonerId.java
@@ -4,6 +4,29 @@ import lombok.Data;
 
 @Data
 public class EncryptedSummonerId {
+    private String queueType;
+    private String summonerName;
+    private boolean hotStreak;
+    private MiniSeriesDTO  miniSeries;
+    private int wins;
+    private boolean veteran;
+    private int losses;
+    private String rank;
+    private String leagueId;
+    private boolean inactive;
+    private boolean freshBlood;
+    private String tier;
+    private String summonerId;
+    private int leaguePoints;
+
+    // private String leagueName;
+    // private String position;
 
 
+    public static class MiniSeriesDTO {
+        public String progress;
+        public int losses;
+        public int target;
+        public int wins;
+    }
 }
--- src/main/java/org/cnu/realcoding/riotapi/domain/SummonerName.java
+++ src/main/java/org/cnu/realcoding/riotapi/domain/SummonerName.java
@@ -1,4 +1,16 @@
 package org.cnu.realcoding.riotapi.domain;
+import lombok.Data;
 
+@Data
 public class SummonerName {
+    private int profileIconId;
+    private String name;
+    private String puuid;
+    private long summonerLevel;
+    private long revisionDate;
+    private String id;
+    private String accountId;
+
+
+
 }
